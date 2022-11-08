65b5e54c4b295a5bb3715fac6dc92ac3251aaaff
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2022-11-08 14:59:45 +0100

    Simplify coordinate handling

--- grid_crawler/db.py
+++ grid_crawler/db.py
@@ -15,60 +15,32 @@ from .hash import phash_1d, phash_2d
 
 Base = declarative_base()
 
-grid_dim_coord = Table(
-    "grid_dim_coord",
-    Base.metadata,
-    Column("grid_id", ForeignKey("grid.id")),
-    Column("dim_coord_id", ForeignKey("dim_coord.id")),
-)
 
-
-class DimCoordHashes(NamedTuple):
-    points_hash: str
-    points_phash: int
-    bounds_hash: str
-    bounds_lower_phash: int
-    bounds_upper_phash: int
-
-
-class AuxCoordHashes(NamedTuple):
+class CoordHashes(NamedTuple):
     points_hash: str
     points_phash: int
     bounds_hash: str
 
 
 class GridHashes(NamedTuple):
-    dim_coords: Mapping[DimCoordHashes, str]
-    two_d_coords: Mapping[AuxCoordHashes, str]
+    coords: Mapping[CoordHashes, str]
 
 
-def hash_dim_coord(coord):
+def hash_coord(coord):
     points = coord.points
     points_hash = xxh3_64_hexdigest(points)
-    points_phash = phash_1d(points).hash
-    bounds = coord.bounds
-    if bounds is not None:
-        bounds_hash = xxh3_64_hexdigest(bounds)
-        bounds_lower_phash = phash_1d(bounds[:, 0]).hash
-        bounds_upper_phash = phash_1d(bounds[:, 1]).hash
+    if points.ndim == 1:
+        points_phash = phash_1d(points).hash
+    elif points.ndim == 2:
+        points_phash = phash_2d(points).hash
     else:
-        bounds_hash = None
-        bounds_lower_phash = None
-        bounds_upper_phash = None
-    return DimCoordHashes(points_hash, points_phash, bounds_hash,
-                          bounds_lower_phash, bounds_upper_phash)
-
-
-def hash_aux_coord(coord):
-    points = coord.points
-    points_hash = xxh3_64_hexdigest(points)
-    points_phash = phash_2d(points).hash
+        points_hash = None
     bounds = coord.bounds
     if bounds is not None:
         bounds_hash = xxh3_64_hexdigest(bounds)
     else:
         bounds_hash = None
-    return AuxCoordHashes(points_hash, points_phash, bounds_hash)
+    return CoordHashes(points_hash, points_phash, bounds_hash)
 
 
 def hash_grid(cube):
@@ -82,19 +54,16 @@ def hash_grid(cube):
         dims[ax] = cube.coord_dims(axis_dim_coords[0])[0]
         axis_non_dim_coords = cube.coords(axis=ax, dim_coords=False)
         non_dim_coords[ax] = axis_non_dim_coords
-    dim_coord_hashes = {
-        hash_dim_coord(coord): coord
-        for coord in dim_coords.values()
+    coord_hashes = {
+        hash_coord(coord): coord
+        for coord in set(dim_coords.values())
+        | set(sum(non_dim_coords.values(), []))
     }
-    two_d_coord_hashes = {
-        hash_aux_coord(coord): coord
-        for coord in set(sum(non_dim_coords.values(), []))
-    }
-    return GridHashes(dim_coord_hashes, two_d_coord_hashes)
+    return GridHashes(coord_hashes)
 
 
-class DimCoord(Base):
-    __tablename__ = "dim_coord"
+class Coord(Base):
+    __tablename__ = "coord"
     __table_args__ = (UniqueConstraint("points_hash", "bounds_hash"), )
 
     id = Column(Integer, primary_key=True)
@@ -103,100 +72,46 @@ class DimCoord(Base):
     points_phash = Column(Integer, nullable=False)
     bounds = Column(PickleType)
     bounds_hash = Column(Integer)
-    bounds_lower_phash = Column(Integer)
-    bounds_upper_phash = Column(Integer)
 
     def __repr__(self):
-        return f"DimCoord({self.id})"
+        return f"Coord({self.id})"
 
 
-grid_two_d_coord = Table(
-    "grid_two_d_coord",
+grid_coord = Table(
+    "grid_coord",
     Base.metadata,
     Column("grid_id", ForeignKey("grid.id")),
-    Column("two_d_coord_id", ForeignKey("two_d_coord.id")),
+    Column("coord_id", ForeignKey("coord.id")),
 )
-
-
-class TwoDCoord(Base):
-    __tablename__ = "two_d_coord"
-    __table_args__ = (UniqueConstraint("points_hash", "bounds_hash"), )
-
-    id = Column(Integer, primary_key=True)
-    points = Column(PickleType, nullable=False)
-    points_hash = Column(Integer, nullable=False)
-    points_phash = Column(Integer, nullable=False)
-    bounds = Column(PickleType)
-    bounds_hash = Column(Integer)
-
-    def __repr__(self):
-        return f"TwoDCoord({self.id})"
 
 
 class Grid(Base):
     __tablename__ = "grid"
 
     id = Column(Integer, primary_key=True)
-    dim_coords = relationship("DimCoord",
-                              secondary=grid_dim_coord,
-                              backref="grids")
-    two_d_coords = relationship("TwoDCoord",
-                                secondary=grid_two_d_coord,
-                                backref="grids")
+    coords = relationship("Coord", secondary=grid_coord, backref="grids")
 
     def __init__(self, cube, session, grid_hashes=None):
         if grid_hashes is None:
             grid_hashes = hash_grid(cube)
-        dim_coords = []
-        for candidate, coord in grid_hashes.dim_coords.items():
+        coords = []
+        for candidate, coord in grid_hashes.coords.items():
             existing = session.scalar(
-                select(DimCoord).where(
-                    DimCoord.points_hash == candidate.points_hash,
-                    DimCoord.bounds_hash == candidate.bounds_hash,
+                select(Coord).where(
+                    Coord.points_hash == candidate.points_hash,
+                    Coord.bounds_hash == candidate.bounds_hash,
                 ))
             if existing is None:
-                dim_coords.append(
-                    DimCoord(points=coord.points,
-                             bounds=coord.bounds,
-                             **candidate._asdict()))
+                coords.append(
+                    Coord(points=coord.points,
+                          bounds=coord.bounds,
+                          **candidate._asdict()))
             else:
-                dim_coords.append(existing)
-        two_d_coords = []
-        for candidate, coord in grid_hashes.two_d_coords.items():
-            existing = session.scalar(
-                select(TwoDCoord).where(
-                    TwoDCoord.points_hash == candidate.points_hash,
-                    TwoDCoord.bounds_hash == candidate.bounds_hash,
-                ))
-            if existing is None:
-                two_d_coords.append(
-                    TwoDCoord(points=coord.points,
-                              bounds=coord.bounds,
-                              **candidate._asdict()))
-            else:
-                two_d_coords.append(existing)
-        # unique_dim_coords.append(candidate)
-        # unique_two_d_coords = []
-        # for coord in set(sum(non_dim_coords.values(), [])):
-        #     candidate =
-        #     existing = session.scalar(select(TwoDCoord).where(
-        #         TwoDCoord.points_hash == candidate.points_hash,
-        #         TwoDCoord.bounds_hash == candidate.bounds_hash))
-        #     if existing is None:
-        #         candidate = TwoDCoord(
-        #             points=coord.points,
-        #             bounds=coord.bounds,
-        #             **candidate._asdict())
-        #     else:
-        #         candidate = existing
-        #     unique_two_d_coords.append(candidate)
-        super().__init__(
-            dim_coords=dim_coords,
-            two_d_coords=two_d_coords,
-        )
+                coords.append(existing)
+        super().__init__(coords=coords)
 
     def __repr__(self):
-        return f"Grid([{', '.join([str(c) for c in self.dim_coords])}])"
+        return f"Grid([{', '.join([str(c) for c in self.coords])}])"
 
 
 class File(Base):
@@ -211,11 +126,10 @@ class File(Base):
     def __init__(self, path, session):
         cube = iris.load_cube(path)
         candidate = hash_grid(cube)
-        # import pdb; pdb.set_trace()
         # existing = session.scalar(
-        #     select(Grid).join(Grid.dim_coords).join(Grid.two_d_coords).where(
-        #         Grid.dim_coords.points_hash.in_(
-        # [c.points_hash for c in candidate.dim_coords])
+        #     select(Grid).join(Grid.coords).where(
+        #         Grid.coords.points_hash.in_(
+        #             [c.points_hash for c in candidate.coords])
         #     ))
         # existing = session.get(Grid, candidate.id)
         existing = None
